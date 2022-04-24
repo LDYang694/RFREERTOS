8c746a32e175c2cac78fe376f5407a66ead94f18
李东阳 <dy-li19@mails.tsinghua.edu.cn>
2022-04-24 22:01:15 +0800

    add prep.sh

---
+++ prep.sh
@@ -0,0 +1,5 @@
+cd /usr/local
+wget https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.08/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
+tar xzvf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
+mv riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14 riscv64-unknown-elf-gcc
+export PATH="/usr/local/riscv64-unknown-elf-gcc/bin:$PATH"
