17a6a3ebeaf534a532d8e636a904f0ef74416de2
zhouyh19 <zhouyh19@users.noreply.github.com>
2022-05-06 15:51:52 +0000

    deploy: 4dd4c255e5dcd07a07dc4ca2685aa994e4fe00ae

---
+++ r_freertos/kernel/event_group/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"constant":[["eventUNBLOCKED_DUE_TO_BIT_SET",""]],"fn":[["vEventGroupDelete",""]],"struct":[["EventGroupDefinition",""]],"type":[["EventBits",""],["EventGroupHandle",""]]});
