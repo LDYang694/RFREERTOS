86bef462a4736e821428cbbfcdec43a753f01f94
李东阳 <dy-li19@mails.tsinghua.edu.cn>
2022-06-08 17:22:55 +0800

    fix ci link:

--- prep.sh
+++ prep.sh
@@ -1,4 +1,5 @@
-wget https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.08/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
+# wget https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.08/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
+wget https://cloud.tsinghua.edu.cn/f/2e74afbba2914efab4d0/?dl=1 -O riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
 tar xzvf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
 mv riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14 riscv64-unknown-elf-gcc
 export PATH="$PWD/riscv64-unknown-elf-gcc/bin:$PATH"
