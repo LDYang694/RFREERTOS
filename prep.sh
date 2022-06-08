42d4a575d4e3d146d1b62c921200ab9807c1bdbe
李东阳 <dy-li19@mails.tsinghua.edu.cn>
2022-06-08 18:03:30 +0800

    test11

--- prep.sh
+++ prep.sh
@@ -2,4 +2,3 @@
 wget https://cloud.tsinghua.edu.cn/f/2e74afbba2914efab4d0/?dl=1 -O riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
 tar xzvf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
 mv riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14 riscv64-unknown-elf-gcc
-export PATH="$PWD/riscv64-unknown-elf-gcc/bin:$PATH"
